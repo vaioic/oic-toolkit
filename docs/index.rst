7e10bbc2aaaea63daa0efbf3f09d2c4f6acfc5fd
Jian Tay <jian.tay@vai.org>
2026-07-08 15:31:43 -0400

    Updating documentation

--- docs/index.rst
+++ docs/index.rst
@@ -1,6 +1,52 @@
 Welcome to the OIC Toolkit Wiki!
 ======================================
 
+The ``oic-toolkit`` is a consolidated Python package of useful image analysis functions 
+developed by the Van Andel Institute Optical Imaging Core. The toolkit features a blend
+of custom algorithms and workflows derived from scikit-image and scipy.
+
+Features
+--------
+
+* Distance-based watershed algorithms
+* Generating annotated plots to visualize segmentation results
+* Stitching of tiled images
+* ... and more!
+
+Getting Started
+---------------
+
+Installation
+~~~~~~~~~~~~
+
+```bash
+pip install oic-toolkit
+```
+
+Usage
+~~~~~
+
+```python
+from oic_toolkit import *
+```
+
+Modules
+^^^^^^^
+
+The toolkit is organized into modular components that address specific bottlenecks in bioimage workflows:
+
+* ``io.py``: Efficient reading and writing of standardized microscopy
+  formats, including multi-resolution pyramidal OME-TIFFs.
+
+* ``register.py``: Algorithms optimized for structural alignment, including sequential tissue section registration (e.g., "Swiss roll" intestinal tissue preparations).
+
+* ``segment.py``: Repeatable tissue and cellular segmentation workflows leveraging morphology tools and watershed algorithms.
+
+* ``display``: Standardized plotting functions and core array manipulations like downsampling, cropping, and contrast normalization.
+
+Content Navigation
+------------------
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
--- src/oic_toolkit/util.py
+++ src/oic_toolkit/util.py
@@ -6,14 +6,15 @@ import skimage as sk
 
 def downsample_image(image, output_path=None, ds_factor=8, normalize=False):
     """
-    Downsample and optically save an image
+    Downsample and optionally save an image.
 
     Parameters
     ----------
     image : ndarray
         Input image
     output_path : str or Path, optional
-        Output path to save downsampled image, by default None. If None, the output image will be returned as a variable instead.
+        Output path to save downsampled image, by default None. If None, the output
+        image will be returned as a variable instead.
     ds_factor : int, optional
         Downsample factor, by default 8
     normalize : bool, optional
@@ -29,32 +30,31 @@ def downsample_image(image, output_path=None, ds_factor=8, normalize=False):
     ValueError
         Error occurs if the input path is not a valid filename (e.g., it is a directory)
     """
-
     image_ds = image[::ds_factor, ::ds_factor]
 
     if normalize:
         image_ds = sk.exposure.rescale_intensity(image_ds, out_range=(0.0, 1.0))
 
     if output_path:
-
         if isinstance(output_path, str):
             output_path = Path(output_path)
 
         if not output_path.suffix:
-            raise ValueError(f"The input path is not a valid filename")
-        
+            raise ValueError("The input path is not a valid filename")
+
         if not (output_path.parent).exists():
             (output_path.parent).mkdir(parents=True)
-        
+
         sk.io.imsave(output_path, image_ds)
 
         print(f"Downsampled image saved to {str(output_path)}.")
     else:
         return image_ds
 
+
 def resize_from_center(image, target_shape):
     """
-    Symmetrically pad or crop an image around the center
+    Symmetrically pad or crop an image around the center.
 
     Parameters
     ----------
@@ -68,10 +68,9 @@ def resize_from_center(image, target_shape):
     output : ndarray
         Resized image
     """
-
     img_h, img_w = image.shape[:2]
     tgt_h, tgt_w = target_shape[:2]
-    
+
     if tgt_h > img_h:
         # Grow height: calculate top and bottom padding
         pad_top = (tgt_h - img_h) // 2
@@ -95,15 +94,15 @@ def resize_from_center(image, target_shape):
         pad_left, pad_right = 0, 0
 
     output = image[crop_top:crop_bottom, crop_left:crop_right]
-    
+
     if tgt_h > img_h or tgt_w > img_w:
         # Define padding for height and width axes
         pad_width = ((pad_top, pad_bottom), (pad_left, pad_right))
-        
+
         # Append an empty padding tuple if the image has color channels
         if image.ndim == 3:
             pad_width += ((0, 0),)
-            
-        output = np.pad(output, pad_width=pad_width, mode='constant', constant_values=0)
-        
+
+        output = np.pad(output, pad_width=pad_width, mode="constant", constant_values=0)
+
     return output
